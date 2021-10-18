b880cfc9ed011e313f6850e8b309f50fbadc9641
safees <safeesmoha@gmail.com>
2021-10-18 09:39:34 +0530

    configured initial routing setup

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http"
+import { Routes,RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { LaptopCardComponent } from './laptop/laptop-card/laptop-card.component';
 import { LaptopListComponent } from './laptop/laptop-list/laptop-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LaptopService } from './services/laptop.service';
+import { AddLaptopComponent } from './laptop/add-laptop/add-laptop.component';
 
-
+const appRoutes : Routes = [
+  { path: '' , component : LaptopListComponent},
+  { path: 'add-laptop' , component : AddLaptopComponent}
+]
 @NgModule({
   declarations: [	
     AppComponent,
     LaptopCardComponent,
     LaptopListComponent,
-      NavBarComponent
+    NavBarComponent,
+    AddLaptopComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     LaptopService
---
+++ FrontEnd/src/app/laptop/add-laptop/add-laptop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-laptop',
+  templateUrl: './add-laptop.component.html',
+  styleUrls: ['./add-laptop.component.css']
+})
+export class AddLaptopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
